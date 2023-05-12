27b3f7094ced01d10d416658b96835c08a4ed742
Shadman <shadman.alizada@gmail.com>
2023-05-12 23:18:15 +0400

    feat: add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import Home from "./components/home/Home";
-import MainProvider from '../src/context/MainContext'
-import Header from "./components/Header";
+import { Routes } from 'react-router-dom';
+import MainProvider from '../src/context/MainContext';
+import Header from './layout/Header';
+import Home from './pages/Home';
+import Women from './pages/Women';
+import { Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <MainProvider>
-      <Header />
-      <Home />
-    </MainProvider>
-  );
+    return (
+        <MainProvider>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/women" element={<Women />} />
+            </Routes>
+        </MainProvider>
+    );
 }
 
 export default App;
