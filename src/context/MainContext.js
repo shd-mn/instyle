c43dd5427e033a70cba234545916f192e7549d43
Shadman <shadman.alizada@gmail.com>
2023-05-12 23:24:37 +0400

    feat: use json-server

--- src/context/MainContext.js
+++ src/context/MainContext.js
@@ -1,20 +1,23 @@
-import { createContext, useContext, useState } from 'react';
-// import axios from 'axios';
+import { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 const MainContext = createContext();
 
 const MainProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
+    const [currency, setCurrency] = useState('USD');
 
-    /* useEffect(() => {
+    useEffect(() => {
         axios
             .get('http://localhost:3010/products')
             .then((res) => setProducts(res.data))
             .catch((err) => console.log(err));
-    }, []); */
+    }, []);
 
     const data = {
         products,
         setProducts,
+        currency,
+        setCurrency,
     };
 
     return <MainContext.Provider value={data}>{children}</MainContext.Provider>;
