6ed6d8e366851a97d598b33f65f4d1cab0ce8e49
Shadman <shadman.alizada@gmail.com>
2023-05-20 20:19:54 +0400

    refactor:  ProductCart some changes

--- src/layout/ProductCard.jsx
+++ src/layout/ProductCard.jsx
@@ -7,21 +7,20 @@ import {
 } from 'react-icons/io5';
 import styles from './ProductCard.module.scss';
 
-const addToCart = (e) => {
-    e.preventDefault();
-};
-
 const ProductCard = ({ product }) => {
-    const { currency } = useMyContext();
+    const { name, price, sale, url } = product;
+    const { currency, cart, toggleCart } = useMyContext();
+    const hasCart = cart.some((item) => item.id === product.id);
+
     return (
         <article className={styles['product-card']}>
             <figure className={styles['img-box']}>
-                <img src={product.url[2]} alt={product.name} />
+                <img src={url[0]} alt={name} />
                 <div className={styles['button-group']}>
                     <button
-                        className={styles.btn}
+                        className={`${styles.btn} ${hasCart && styles.active}`}
                         type="button"
-                        onClick={(e) => addToCart(e)}
+                        onClick={(e) => toggleCart(e, product)}
                     >
                         <IoCartOutline size={'20px'} />
                     </button>
@@ -32,17 +31,18 @@ const ProductCard = ({ product }) => {
                         <IoResizeOutline size={'20px'} />
                     </button>
                 </div>
-                {product.sale !== 0 && (
-                    <div className={styles.badge}>
-                        {product.sale.toFixed(2)}
-                    </div>
+                {sale !== 0 && (
+                    <div className={styles.badge}>{sale.toFixed(2)}</div>
                 )}
             </figure>
             <div className={styles['product-text-box']}>
-                <h5>{product.name}</h5>
+                <h5>{name}</h5>
                 <p className={styles['product-price']}>
-                    <span>{convertCurrency(product.price, currency)}</span>
-                    {convertCurrency(product.price - product.sale, currency)}
+                    {sale < 0 && (
+                        <span> {convertCurrency(price, currency)}</span>
+                    )}
+
+                    {convertCurrency(price + sale, currency)}
                     {currency === 'USD' ? '$' : '₼'}
                 </p>
             </div>
