9e9be449ffcaecae0305d658616c5495d2ada489
Shadman <shadman.alizada@gmail.com>
2023-05-20 20:20:45 +0400

    refactor: Women Page

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import PageTitle from '../layout/PageTitle';
+import { useLocation } from 'react-router-dom';
+import Products from '../container/Products/Products';
+import { useMyContext } from '../context/MainContext';
 const Women = () => {
-    return <div>Women</div>;
+    const { pathname } = useLocation();
+    const { products } = useMyContext();
+    const womenProducts = products.filter(
+        (product) => product.category === 'women'
+    );
+    return (
+        <div>
+            <PageTitle title="Women" pathname={pathname} />
+            <Products products={womenProducts} />
+        </div>
+    );
 };
 
 export default Women;
