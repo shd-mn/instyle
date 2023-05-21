e25f016a7c11fb44adc80d8fe772c17ad86c55d4
Shadman <shadman.alizada@gmail.com>
2023-05-22 00:06:28 +0400

    feat: adding size and color filter, use custom hook for fetch data

--- src/container/Products/Products.jsx
+++ src/container/Products/Products.jsx
@@ -1,21 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useMyContext } from '../../context/MainContext';
 import ProductCard from '../../layout/ProductCard';
+import Filter from './Filter';
 import styles from './Products.module.scss';
-const Products = ({ products }) => {
+const Products = ({ products, isLoading, error }) => {
+    const { isChecked } = useMyContext();
+
+    const filtered = products.filter((item) => {
+        const { color, size } = isChecked;
+
+        return (
+            color.every((c) => item.color.includes(c)) &&
+            size.every((c) => item.size.includes(c))
+        );
+    });
+
     return (
-        <div>
-            <section>
-                <div className="container">
-                    <div className={styles.products}>
-                        {products.map((product) => (
-                            <Link key={product.id}>
-                                <ProductCard product={product} />
-                            </Link>
+        <section>
+            <div className="container">
+                <div className={styles.products}>
+                    <Filter products={products} />
+                    <div className={styles['products-list']}>
+                        {filtered?.map((product) => (
+                            <ProductCard key={product.id} product={product} />
                         ))}
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 };
 export default Products;
--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import PageTitle from '../layout/PageTitle';
 import { useLocation } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+import PageTitle from '../layout/PageTitle';
 import Products from '../container/Products/Products';
 import { useMyContext } from '../context/MainContext';
 const Women = () => {
     const { pathname } = useLocation();
-    const { products } = useMyContext();
-    const womenProducts = products.filter(
-        (product) => product.category === 'women'
-    );
+    const { isChecked } = useMyContext();
+    const {
+        data: products,
+        isLoading,
+        error,
+    } = useFetch('products', '*', 'category=eq.women', '0-17');
+    // & price=gt.90 & price=lte.138
+
     return (
         <div>
             <PageTitle title="Women" pathname={pathname} />
-            <Products products={womenProducts} />
+            <p>{JSON.stringify(isChecked)}</p>
+            {console.log()}
+            <Products products={products} isLoading={isLoading} error={error} />
         </div>
     );
 };
