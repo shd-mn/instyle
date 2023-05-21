7b5e50319b180aaee2dd95267ff5f55822254ab0
Shadman <shadman.alizada@gmail.com>
2023-05-22 00:07:05 +0400

    some changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { Route } from 'react-router-dom';
 import Header from './layout/Header';
 import Home from './pages/Home';
 import Women from './pages/Women';
+import SingleProduct from './pages/SingleProduct';
 import NewsPage from './pages/NewsPage';
 import SingleNews from './container/News/SingleNews';
 import NotFound from './pages/NotFound';
+import WishlistPage from './pages/WishlistPage';
 import CartPage from './pages/CartPage';
 
 function App() {
@@ -16,10 +18,12 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/women" element={<Women />} />
+                <Route path="/product/:id" element={<SingleProduct />} />
                 <Route path="/news">
                     <Route index element={<NewsPage />} />
                     <Route path=":id" element={<SingleNews />} />
                 </Route>
+                <Route path="/wishlist" element={<WishlistPage />} />
                 <Route path="/cart" element={<CartPage />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
