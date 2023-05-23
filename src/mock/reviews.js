ed2eaa0d2e83435cacf92f6b2f213637083790a9
Shadman <shadman.alizada@gmail.com>
2023-05-23 23:54:30 +0400

    some changes

---
+++ src/mock/reviews.js
@@ -0,0 +1,31 @@
+export const reviews = [
+    {
+        id: 1,
+        name: 'N** M**',
+        rating: '5',
+        size: 'S',
+        color: 'red',
+        date: '24 May 2023',
+        comment:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid et iste nemo incidunt eos, sint vitae mollitia harum doloribus quia.',
+    },
+    {
+        id: 2,
+        name: 'C** D**',
+        rating: '4.3',
+        size: 'L',
+        color: 'blue',
+        date: '15 March 2023',
+        comment:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe laboriosam dignissimos commodi, exercitationem optio.',
+    },
+    {
+        id: 3,
+        name: 'J** S**',
+        rating: '3.6',
+        size: 'M',
+        color: 'black',
+        date: '15 April 2023',
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+];
