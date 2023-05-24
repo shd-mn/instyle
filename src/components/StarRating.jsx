f4bfdc08306f012a81bdcdfa080b9f9e77bcffdd
Shadman <shadman.alizada@gmail.com>
2023-05-24 23:53:02 +0400

    refactor: fix StarRating code key warning

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,25 +1,19 @@
 import { IoStarOutline, IoStarHalfOutline, IoStarSharp } from 'react-icons/io5';
 
-export const StarRating = (rating) => {
-    const MAX_RATING = 5;
-    const filledStars = Math.floor(rating);
-    const hasHalfStar = rating % 1 !== 0;
-    const emptyStars = MAX_RATING - filledStars - (hasHalfStar ? 1 : 0);
-
-    return (
-        <span>
-            {/* Filled stars */}
-            {Array(filledStars).fill(
-                <IoStarSharp fill="#FFD700" stroke="gray" />
-            )}
-
-            {/* Half star */}
-            {hasHalfStar && <IoStarHalfOutline fill="#FFD700" stroke="gray" />}
-
-            {/* Empty stars */}
-            {Array(emptyStars).fill(
-                <IoStarOutline fill="#FFD700" stroke="gray" />
-            )}
-        </span>
-    );
+export const StarRating = (stars) => {
+    const tempStars = Array.from({ length: 5 }, (_, index) => {
+        const number = index + 0.5;
+        return (
+            <span key={index}>
+                {stars > number ? (
+                    <IoStarSharp fill="#FFD700" stroke="#FFD700" />
+                ) : stars > index ? (
+                    <IoStarHalfOutline fill="#FFD700" stroke="#FFD700" />
+                ) : (
+                    <IoStarOutline fill="#FFD700" stroke="#FFD700" />
+                )}
+            </span>
+        );
+    });
+    return <>{tempStars}</>;
 };
