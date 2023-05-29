75f94bc582986618dcf53e59f5255b0a24c12378
Shadman <shadman.alizada@gmail.com>
2023-05-29 23:43:43 +0400

    refactor: CardSkleton for Category comp

--- src/layout/CardSkeleton.jsx
+++ src/layout/CardSkeleton.jsx
@@ -1,20 +1,35 @@
 import Skeleton from 'react-loading-skeleton';
-import styles from './ProductCard.module.scss';
 const CardSkeleton = ({ count }) => {
-    return Array(count)
-        .fill(0)
-        .map((_, index) => (
-            <div key={index} className={styles['product-card']}>
-                <div className="skeleton">
-                    <Skeleton containerClassName="flex-1" height={400} />
-                </div>
-                <div className="skeleton skleton-text">
-                    <Skeleton containerClassName="flex-1" height={20} width={200} />
-                </div>
-                <div className="skeleton skleton-text">
-                    <Skeleton containerClassName="flex-1" height={20} width={160} />
-                </div>
-            </div>
-        ));
+    return (
+        <div className="skeleton-container">
+            {Array(count)
+                .fill(0)
+                .map((_, index) => (
+                    <div key={index}>
+                        <div className="skeleton">
+                            <Skeleton
+                                containerClassName="flex-1"
+                                height={350}
+                                width="100%"
+                            />
+                        </div>
+                        <div className="skeleton skleton-text">
+                            <Skeleton
+                                containerClassName="flex-1"
+                                height={20}
+                                width="60%"
+                            />
+                        </div>
+                        <div className="skeleton skleton-text">
+                            <Skeleton
+                                containerClassName="flex-1"
+                                height={20}
+                                width={160}
+                            />
+                        </div>
+                    </div>
+                ))}
+        </div>
+    );
 };
 export default CardSkeleton;
