f729d990f52e5a8cdbb6aaf97c07225c5b65e28b
Shadman <shadman.alizada@gmail.com>
2023-06-14 23:59:44 +0400

    feat: added modal image slider btn and indicators

--- src/layout/ProductModal.jsx
+++ src/layout/ProductModal.jsx
@@ -1,14 +1,32 @@
-import { AiOutlineClose } from 'react-icons/ai';
+import { useEffect, useState } from 'react';
 import { useMyContext } from '../context/MainContext';
+
+import { BsXLg, BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import styles from './ProductModal.module.scss';
 
 const ProductModal = () => {
     const { isModalShow, modalContent, closeModal } = useMyContext();
+    const [index, setIndex] = useState(0);
 
-    if (!isModalShow) {
-        return <></>;
-    }
-    const { name, url, color } = modalContent;
+    const handlePrevImg = () => {
+        if (index < 1) {
+            setIndex(modalContent?.length - 1);
+        } else {
+            setIndex((prev) => prev - 1);
+        }
+    };
+    const handleNextImg = () => {
+        if (index >= modalContent?.length - 1) {
+            setIndex(0);
+        } else {
+            setIndex((prev) => prev + 1);
+        }
+    };
+
+    useEffect(() => {
+        setIndex(0);
+    }, [isModalShow]);
+
     return (
         <>
             <div
@@ -17,28 +35,37 @@ const ProductModal = () => {
                 }`}
             >
                 <figure>
-                    <img src={url[0]} alt="product" />
+                    <img src={modalContent[index]} alt="product" />
                 </figure>
-                <div className={styles.content}>
-                    <h3>{name}</h3>
-                    <div className={styles.color}>
-                        {color.map((item) => (
-                            <span
-                                key={item}
-                                className={styles['color-item']}
-                                style={{ background: `${item}` }}
-                            ></span>
-                        ))}
-                    </div>
+                <div className={styles['btn-group']}>
+                    <button type="button" onClick={handlePrevImg}>
+                        <BsChevronLeft />
+                    </button>
+                    <button type="button" onClick={handleNextImg}>
+                        <BsChevronRight />
+                    </button>
                 </div>
                 <button
                     className={styles['close-btn']}
                     type="button"
                     onClick={closeModal}
                 >
-                    <AiOutlineClose />
+                    <BsXLg />
                 </button>
+                <div className={styles['slider-dot']}>
+                    {modalContent.map((_, idx) => (
+                        <button
+                            key={idx}
+                            className={`${styles['dot-btn']} ${
+                                index === idx && styles.active
+                            }`}
+                            type="button"
+                            onClick={() => setIndex(idx)}
+                        ></button>
+                    ))}
+                </div>
             </div>
+
             {isModalShow && (
                 <div
                     className={styles.overlay}
