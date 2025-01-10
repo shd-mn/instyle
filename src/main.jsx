5ae352c17dd78da8d089aa93c3400b4291227de7
Shadman <shadman.alizada@gmail.com>
2025-01-10 19:27:41 +0400

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import MainProvider from './context/MainContext';
 import ProductsProvider from './context/ProductsContext';
 import App from './App';
@@ -15,5 +16,6 @@ createRoot(document.getElementById('root')).render(
         </CartProvider>
       </ProductsProvider>
     </MainProvider>
+    <Analytics />
   </StrictMode>
 );
